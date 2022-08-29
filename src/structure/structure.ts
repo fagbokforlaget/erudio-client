c7f5c35495255390b2a69f8ba29b2e44bdf521ea
Jeril Jose <jeril96@gmail.com>
2022-08-29 09:23:21 +0530

    Codereview changes - naming and optional fields

--- src/structure/structure.ts
+++ src/structure/structure.ts
@@ -1,16 +1,16 @@
 import { apiClient } from '../erudio';
 
 export interface Options {
-  filter: string;
-  page: number;
-  limit: number;
-  sort: string;
+  filter?: string;
+  page?: number;
+  limit?: number;
+  sort?: string;
 }
 
 export class Structure {
   private base_url = 'http://edtech-structure-service.dev.svc.cluster.local';
 
-  public async getData(namespace: string, options: Options): Promise<any> {
+  public async listNodes(namespace: string, options?: Options): Promise<any> {
     const url = this.base_url + `/structures/${namespace}/nodes/`;
     try {
       const res = await apiClient.get(url, { params: options });
