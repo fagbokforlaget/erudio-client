143f7077ecf92a97cf6e8162f82405ff655323bc
Jeril Jose <jeril96@gmail.com>
2022-09-14 14:54:42 +0530

    structure node output dto corrected

--- src/content-fusion/dto/structure-node-output.dto.ts
+++ src/content-fusion/dto/structure-node-output.dto.ts
@@ -1,12 +1,24 @@
 export type Localisation = {
-  readonly id: string;
-  readonly validation_schema_url?: string;
-  readonly service: string;
-  readonly object_id: string;
-  readonly locale: string;
-  readonly content: Record<string, unknown>;
-  readonly created_at: string;
-  readonly updated_at: string;
+  readonly competencyAims: CompetencyAims;
+  readonly globalAudio: GlobalAudio;
+  readonly bannerImage: BannerImage;
+  readonly useAnchorScrollNav: boolean;
+  readonly altText: string;
+  readonly copyright: string;
+  readonly author: string;
+  readonly linkToContentBlock: Record<string, unknown>;
+  readonly relatedLinks: Record<string, unknown>;
+};
+
+export type Content = {
+  readonly competencyAims: CompetencyAims;
+  readonly globalAudio: GlobalAudio;
+  readonly bannerImage: BannerImage;
+  readonly useAnchorScrollNav: boolean;
+  readonly altText: string;
+  readonly copyright: string;
+  readonly author: string;
+  readonly readingTime: number;
 };
 
 export type Tag = {
@@ -14,7 +26,44 @@ export type Tag = {
   readonly keyword: string;
 };
 
+export enum TypeEnum {
+  COURSE = 'COURSE',
+  UNIT = 'UNIT',
+  LESSON = 'LESSON',
+  DIRECTORY = 'DIRECTORY',
+  OBJECT = 'OBJECT',
+}
+
+export type GlobalAudio = {
+  readonly type: string;
+  readonly value: string;
+};
+
+export type Aim = {
+  readonly teacherOnly: boolean;
+  readonly body: string;
+  readonly title: string;
+};
+
+export type CompetencyAims = {
+  readonly aims: Aim[];
+  readonly hasCompetencyAims: boolean;
+};
+
+export type BannerImage = {
+  readonly imageId: string;
+  readonly altText: string;
+  readonly copyright: string;
+};
+
 export class StructureNodeOutputDto {
-  localization: Localisation | Object;
-  readonly tags: Tag;
+  readonly namespace: string;
+  readonly schemaId: string;
+  readonly content: Content;
+  readonly type: TypeEnum;
+  readonly createdAt: string;
+  readonly updatedAt: string;
+  readonly id: string;
+  localization: Record<string, Localisation>;
+  readonly tags: Tag[];
 }
