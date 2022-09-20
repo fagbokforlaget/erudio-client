8692d9d6e1d2d1d72e0375344cc27d690118d5b7
Jeril Jose <jeril96@gmail.com>
2022-09-20 14:52:47 +0530

    Data for testscript

---
+++ test/data/get-structure-node-data.ts
@@ -0,0 +1,54 @@
+export const content = {
+  namespace: '15a85b2d-76b5-46bf-bccb-e5acd969bac9',
+  schemaId: '229434de-872f-486b-ac7a-2b45484579f2',
+  content: {
+    competencyAims: {
+      aims: [
+        {
+          teacherOnly: false,
+          body: '<p>Is</p>',
+          title: 'History',
+        },
+      ],
+      hasCompetencyAims: false,
+    },
+    globalAudio: {
+      type: 'none',
+      value: '',
+    },
+    bannerImage: {
+      imageId: '73d3a77a-5902-45e1-9bc3-d437cf268d20',
+      altText: 'Learn Norwegian English',
+      copyright: 'Fagbokforaget',
+    },
+    useAnchorScrollNav: false,
+    altText: 'Introduction',
+    copyright: 'Intro',
+    author: 'Intro',
+    readingTime: 1800,
+  },
+  type: 'OBJECT',
+  createdAt: '2022-09-14T08:13:38.365Z',
+  updatedAt: '2022-09-14T08:15:29.275Z',
+  id: 'b942d4de-921c-4406-abbd-464dabb7b210',
+  tags: [],
+  localization: {
+    en: {
+      competencyAims: {
+        aims: [],
+        hasCompetencyAims: false,
+      },
+      linkToContentBlock: {},
+      relatedLinks: {},
+      globalAudio: {
+        type: 'none',
+        value: '',
+      },
+      bannerImage: {},
+      useAnchorScrollNav: false,
+      altText: 'sdfs',
+      copyright: 'dfsdfs',
+      author: 'sdfsdf',
+    },
+  },
+};
---
+++ test/data/get-structure-nodes.ts
@@ -0,0 +1,25 @@
+export const nodeList = {
+  data: [
+    {
+      id: 'sample',
+      name: 'sample',
+      slug: 'sample',
+      namespace: 'sample',
+      description: 'sample',
+      parent: 'sample',
+      cover: 'sample',
+      order: 45,
+      type: 'sample',
+      contentId: 'sample',
+      contentType: 'sample',
+      createdAt: 'sample',
+      updatedAt: 'sample',
+      accessLevel: 'sample',
+    },
+  ],
+  pagination: {
+    total: 1,
+    page: 1,
+    limit: 1,
+  },
+};
