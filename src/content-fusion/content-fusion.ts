2067eac8f592baf45cdddf15048f8966783933e0
Jeril Jose <jeril96@gmail.com>
2022-11-10 14:43:04 +0530

    Check for object exist

--- src/content-fusion/content-fusion.ts
+++ src/content-fusion/content-fusion.ts
@@ -15,9 +15,11 @@ export class ContentFusion {
     const url = `${this.baseUrl}/content/${contentId}`;
     const res = await new HttpClientProxy().get<Contents>(url);
     if (locale) {
-      const localization = res.localization[locale];
-      res.localization = {};
-      res.localization[locale] = localization;
+      if (res?.localization?.[locale]) {
+        const localization = res.localization[locale];
+        res.localization = {};
+        res.localization[locale] = localization;
+      }
     }
     return res;
   }
