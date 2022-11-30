24ce7e2cd19d3c5588aa3a7d0ffee95e56c3fb6d
Jacek Martuzalski <jmartuzalski@sii.pl>
2022-11-30 14:25:46 +0100

    fix: simplified export

--- index.ts
+++ index.ts
@@ -10,4 +10,4 @@ export {
   Contents,
   Tag,
 } from './src/content-fusion/dto/content-node-dto';
-export { StructureLink as StructureLink } from './src/structure-link/dto/structure-link';
+export { StructureLink } from './src/structure-link/dto/structure-link';
